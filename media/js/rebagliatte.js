34327044ad217853c2b5aa4261299147bc73458e
Veronica Rebagliatte <rebagliatte@gmail.com>
2011-04-16 01:42:04 -0300

    Skills, portfolio and contact details

---
+++ media/js/rebagliatte.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	// Fancybox
+	$("#portfolio a").fancybox({
+		'speedIn'		:	600, 
+		'speedOut'		:	300, 
+		'transitionIn' : 'elastic',
+		'transitionOut' : 'elastic',
+		'opacity' : true,
+		'overlayShow' : false,
+		'titlePosition' : 'inside' 
+	});
+	// Quotes Rotator
+	
+});

cd4316d5846cbbdc918d7b84792eb0fc45703d55
Verónica Rebagliatte <rebagliatte@gmail.com>
2011-12-02 14:49:38 -0200

    Wireframe for DAL

---
+++ sandbox/DAL/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,110 @@
+﻿
+var PageName = 'Home';
+var PageId = 'bb22c5a824c34e65a5f386aae38fbeb0'
+var PageUrl = 'Home.html'
+document.title = 'Home';
+var PageNotes = 
+{
+"pageName":"Home",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '2');
+  value = value.replace(/\[\[GenMonth\]\]/g, '12');
+  value = value.replace(/\[\[GenMonthName\]\]/g, 'December');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, 'Friday');
+  value = value.replace(/\[\[GenYear\]\]/g, '2011');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u20 = document.getElementById('u20');
+
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'top';
+var u22 = document.getElementById('u22');
+gv_vAlignTable['u22'] = 'top';
+var u23 = document.getElementById('u23');
+gv_vAlignTable['u23'] = 'top';
+var u24 = document.getElementById('u24');
+gv_vAlignTable['u24'] = 'top';
+var u25 = document.getElementById('u25');
+gv_vAlignTable['u25'] = 'top';
+var u26 = document.getElementById('u26');
+gv_vAlignTable['u26'] = 'top';
+var u27 = document.getElementById('u27');
+gv_vAlignTable['u27'] = 'top';
+var u28 = document.getElementById('u28');
+gv_vAlignTable['u28'] = 'top';
+var u0 = document.getElementById('u0');
+
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u2 = document.getElementById('u2');
+
+var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'center';
+var u4 = document.getElementById('u4');
+
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'center';
+var u6 = document.getElementById('u6');
+
+var u7 = document.getElementById('u7');
+gv_vAlignTable['u7'] = 'center';
+var u8 = document.getElementById('u8');
+
+var u9 = document.getElementById('u9');
+gv_vAlignTable['u9'] = 'center';
+var u10 = document.getElementById('u10');
+
+var u11 = document.getElementById('u11');
+gv_vAlignTable['u11'] = 'center';
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'top';
+var u13 = document.getElementById('u13');
+gv_vAlignTable['u13'] = 'top';
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'top';
+var u15 = document.getElementById('u15');
+gv_vAlignTable['u15'] = 'top';
+var u16 = document.getElementById('u16');
+gv_vAlignTable['u16'] = 'top';
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'top';
+var u18 = document.getElementById('u18');
+gv_vAlignTable['u18'] = 'top';
+var u19 = document.getElementById('u19');
+
+if (window.OnLoad) OnLoad();
---
+++ sandbox/DAL/data/sitemap.js
@@ -0,0 +1,20 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Home",
+"type":"Wireframe",
+"url":"Home.html",
+"children":[
+{
+"pageName":"Page 1",
+"type":"Wireframe",
+"url":"Page_1.html"},
+{
+"pageName":"Page 2",
+"type":"Wireframe",
+"url":"Page_2.html"},
+{
+"pageName":"Page 3",
+"type":"Wireframe",
+"url":"Page_3.html"}]}]};

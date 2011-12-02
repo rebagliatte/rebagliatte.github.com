07dfd11661dbe0370d1806b7152c01bb27d3b5db
Verónica Rebagliatte <rebagliatte@gmail.com>
2011-12-02 16:07:23 -0200

    Progress on DAL wireframes

---
+++ sandbox/DAL/data/sitemap.js
@@ -0,0 +1,20 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Home",
+"type":"Wireframe",
+"url":"Home.html",
+"children":[
+{
+"pageName":"Leyes",
+"type":"Wireframe",
+"url":"Leyes.html"},
+{
+"pageName":"Parlamentarios",
+"type":"Wireframe",
+"url":"Parlamentarios.html"},
+{
+"pageName":"Page 3",
+"type":"Wireframe",
+"url":"Page_3.html"}]}]};

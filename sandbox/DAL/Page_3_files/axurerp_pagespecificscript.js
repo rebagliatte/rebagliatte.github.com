bb8dfe45a824a0e8fa2e1dda4408e3cbc842ed12
Verónica Rebagliatte <rebagliatte@gmail.com>
2011-12-02 16:12:14 -0200

    Progress on DAL wireframes

---
+++ sandbox/DAL/Page_3_files/axurerp_pagespecificscript.js
@@ -0,0 +1,66 @@
+﻿
+var PageName = 'Page 3';
+var PageId = '9735842729564da0afc1912658623753'
+var PageUrl = 'Page_3.html'
+document.title = 'Page 3';
+var PageNotes = 
+{
+"pageName":"Page 3",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '2');
+  value = value.replace(/\[\[GenMonth\]\]/g, '12');
+  value = value.replace(/\[\[GenMonthName\]\]/g, 'December');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, 'Friday');
+  value = value.replace(/\[\[GenYear\]\]/g, '2011');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u0 = document.getElementById('u0');
+
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u2 = document.getElementById('u2');
+
+var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'center';
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'top';
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'top';
+var u6 = document.getElementById('u6');
+
+if (window.OnLoad) OnLoad();
